42d4cd9ebc4312fef95765fa2b24d8de8fc60114
mingyos0ng <mingyo.song56@myhunter.cuny.edu>
2026-04-13 16:30:39 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+// make the html elements global because we might want to manipulate them throughout our sketches
+let headerElement;
+let slider;
+let inputText;
+let dropdown;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+
+  headerElement = createElement("h2", "hi this is an h2");
+  headerElement.position(0, 0);
+  headerElement.textContent = "i have changed the content now";
+
+  // createSlider (min, max, [value], [step])
+  // value = starting value
+  // step = if you want it to snap to a value
+  slider = createSlider(0, width, width / 2);
+
+  // createInput ([value])
+  inputText = createInput("this is some text");
+  inputText.position(10, 350);
+
+  //createSelect ([multiple])
+  dropdown = createSelect();
+  dropdown.option("sky blue", "skyblue");
+  dropdown.option("pink");
+}
+
+function draw() {
+  background(dropdown.selected());
+
+  // print(slider.value)
+  text("this is text inside the canvas", width / 2, height / 2);
+}
